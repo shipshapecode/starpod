1443706a1b24f9d60a40313c0d95abdcc80764d1
Robert Wagner <rwwagner90@gmail.com>
2024-07-13 21:31:38 -0400

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,15 +1,15 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
-  const name = data.get("name");
-  const email = data.get("email");
-  const message = data.get("message");
+  const name = data.get('name');
+  const email = data.get('email');
+  const message = data.get('message');
   // Validate the data - you'll probably want to do more than this
   if (!name || !email || !message) {
     return new Response(
       JSON.stringify({
-        message: "Missing required fields",
+        message: 'Missing required fields'
       }),
       { status: 400 }
     );
@@ -17,7 +17,7 @@ export const POST: APIRoute = async ({ request }) => {
   // Do something with the data, then return a success response
   return new Response(
     JSON.stringify({
-      message: "Success!"
+      message: 'Success!'
     }),
     { status: 200 }
   );
