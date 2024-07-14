e68b37bba9ca835f449aa9e16bb3c81ad961ba75
Robert Wagner <rwwagner90@gmail.com>
2024-07-13 21:31:15 -0400

    Start on contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'preact/hooks';
+
+export default function ContactForm() {
+  const [responseMessage, setResponseMessage] = useState('');
+
+  async function submit(e: SubmitEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      body: formData
+    });
+    const data = await response.json();
+    if (data.message) {
+      setResponseMessage(data.message);
+    }
+  }
+
+  return (
+    <form class="flex flex-col gap-2" onSubmit={submit}>
+      <input
+        class="input"
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Enter your name"
+        required
+      />
+      <input
+        class="input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Enter your email"
+        required
+      />
+
+      <textarea
+        class="input"
+        id="message"
+        name="message"
+        placeholder="Write a message"
+        required
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,11 +15,19 @@ export default {
         'dark-button': 'oklch(28.08% 0.048 281.19 / <alpha-value>)',
         'dark-card': 'oklch(18.74% 0.024 280.08 / <alpha-value>)',
         'dark-icon': 'oklch(71.44% 0.019 264.45 / <alpha-value>)',
+        'dark-input-bg': 'oklch(21.63% 0.031 280.68 / <alpha-value>)',
+        'dark-input-border': 'oklch(28.08% 0.048 281.19 / <alpha-value>)',
+        'dark-input-border-focused':
+          'oklch(45.84% 0.078 281.04 / <alpha-value>)',
         'dark-player': 'oklch(22.61% 0.035 280.78 / <alpha-value>)',
         'dark-text-body': 'oklch(61.91% 0.019 264.42 / <alpha-value>)',
 
         'light-card': 'oklch(97.65% 0.005 274.97 / <alpha-value>)',
         'light-icon': 'oklch(67.65% 0.043 285.21 / <alpha-value>)',
+        'light-input-bg': 'oklch(100% 0 0 / <alpha-value>)',
+        'light-input-border': 'oklch(88.09% 0.018 234.53 / <alpha-value>)',
+        'light-input-border-focused':
+          'oklch(57.79% 0.03 234.37 / <alpha-value>)',
         'light-player': 'oklch(95.94% 0.009 279.69 / <alpha-value>)',
         'light-text-body': 'oklch(59.68% 0.017 285.89 / <alpha-value>)',
         'light-text-heading': 'oklch(30.55% 0.052 292.57 / <alpha-value>)'
@@ -34,5 +42,9 @@ export default {
       }
     }
   },
-  plugins: [require('tailwindcss-oklch')(), require('@tailwindcss/typography')]
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwindcss-oklch')(),
+    require('@tailwindcss/typography')
+  ]
 };
