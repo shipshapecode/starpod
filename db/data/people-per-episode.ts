f412a919a4d8f3e8642cd8b9e4bd02c6f151f83c
Robert Wagner <rwwagner90@gmail.com>
2024-07-25 19:48:00 -0400

    Add more data to seed

--- db/data/people-per-episode.ts
+++ db/data/people-per-episode.ts
@@ -1,8 +1,31 @@
 export default {
+  // 152
+  'the-future-of-ember-and-modern-build-tools-with-chris-manson': [
+    { id: 'robbiethewagner', host: true },
+    { id: 'mansona', host: false },
+  ],
   // 151
   'edge-browser-and-pwas-a-new-era-in-web-development-with-diego-gonzales': [
+    { id: 'chuckcarpenter', host: true },
     { id: 'argyleink', host: true },
-    { id: 'chuckcarpenter', host: true }
-  ]
+    { id: 'diegogonzales', host: false },
+  ],
   // 150
+  'secrets-of-a-javascript-guru-natalia-venditto-on-ai-graphql-and-more': [
+    { id: 'chuckcarpenter', host: true },
+    { id: 'argyleink', host: true },
+    { id: 'nvenditto', host: false },
+  ],
+  // 149
+  'exploring-bevvys-whiskey-app-world-with-nas-tonchev': [
+    { id: 'robbiethewagner', host: true },
+    { id: 'chuckcarpenter', host: true },
+    { id: 'ntonchev', host: false },
+  ],
+  // 148
+  'understanding-whiskey-with-prime-barrels-michael-nagdi': [
+    { id: 'robbiethewagner', host: true },
+    { id: 'chuckcarpenter', host: true },
+    { id: 'mnagdi', host: false },
+  ],
 } as { [key: string]: Array<{ id: string; host?: boolean }> };
--- db/data/people.ts
+++ db/data/people.ts
@@ -2,8 +2,33 @@ export default [
   { id: 'argyleink', name: 'Adam Argyle', img: '' },
   { id: 'chuckcarpenter', name: 'Charles William Carpenter III', img: '' },
   {
+    id: 'diegogonzales',
+    name: 'Diego Gonzales',
+    img: ''
+  },
+  {
     id: 'kvlly',
     name: 'Kelly Vaughn',
+    img: ''
+  },
+  {
+    id: 'mansona',
+    name: 'Chris Manson',
+    img: ''
+  },    
+  {
+    id: 'mnagdi',
+    name: 'Michael Nagdi',
+    img: ''
+  },  
+  {
+    id: 'ntonchev',
+    name: 'Nas Tonchev',
+    img: ''
+  },  
+  {
+    id: 'nvenditto',
+    name: 'Natalia Venditto',
     img: ''
   },
   {
