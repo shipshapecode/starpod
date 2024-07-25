5f47f39e5041b28dd0230e8e55ec33bc7ceeb3ca
Robert Wagner <rwwagner90@gmail.com>
2024-07-25 09:32:25 -0400

    Atropos tweaks, create FullPlayButton

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -1,47 +1,14 @@
 import type { JSX } from 'preact/jsx-runtime';
 import { useState } from 'preact/hooks';
-import { currentEpisode, isPlaying } from '../components/state';
-import type { Episode } from '../lib/rss';
 import FormattedDate from '../components/FormattedDate';
+import FullPlayButton from '../components/FullPlayButton';
+import { currentEpisode } from '../components/state';
+import type { Episode } from '../lib/rss';
 
 type Props = {
   episodes: Array<Episode>;
   url: URL;
 };
-
-const playIcon = (
-  <svg
-    class="h-9 w-9"
-    fill="currentColor"
-    viewBox="0 0 20 20"
-    xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-      clip-rule="evenodd"
-    />
-  </svg>
-);
-
-const pauseIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    class="h-9 w-9"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
-      clip-rule="evenodd"
-    />
-  </svg>
-);
 
 function renderIcon(icon: JSX.Element) {
   return <span>{icon}</span>;
@@ -84,7 +51,7 @@ export default function EpisodeList({ episodes, url }: Props) {
                 <img
                   alt={`${episode.title} - episode art`}
                   aria-hidden="true"
-                  class="block h-20 mb-1 w-20 rounded-md lg:mr-6"
+                  class="mb-1 block h-20 w-20 rounded-md lg:mr-6"
                   height={80}
                   src={episode.episodeImage ?? '/images/www.png'}
                   width={80}
@@ -102,34 +69,7 @@ export default function EpisodeList({ episodes, url }: Props) {
                   <p class="mb-5">{episode.description}</p>
 
                   <div class="flex items-center gap-6 text-sm">
-                    <button
-                      class="btn"
-                      onClick={() => {
-                        currentEpisode.value = {
-                          ...episode
-                        };
-
-                        isPlaying.value = isCurrentEpisode
-                          ? !isPlaying.value
-                          : true;
-                      }}
-                    >
-                      <span class="flex w-full items-center rounded-full p-2 pr-4 text-light-text-heading dark:text-white">
-                        <span class="mr-3 w-8 text-light-text-heading dark:text-white">
-                          {isCurrentEpisode && isPlaying.value
-                            ? renderIcon(pauseIcon)
-                            : renderIcon(playIcon)}
-                        </span>
-                        {isCurrentEpisode && isPlaying.value ? 'Pause' : 'Play'}{' '}
-                        Episode
-                        <span class="sr-only">
-                          (press to{' '}
-                          {isCurrentEpisode && isPlaying.value
-                            ? 'pause)'
-                            : 'play)'}
-                        </span>
-                      </span>
-                    </button>
+                    <FullPlayButton episode={episode} />
 
                     <a
                       class="font-bold text-light-text-heading dark:text-white"
---
+++ src/components/FullPlayButton.tsx
@@ -0,0 +1,75 @@
+import type { JSX } from 'preact/jsx-runtime';
+
+import type { Episode } from '../lib/rss';
+import { currentEpisode, isPlaying } from './state';
+
+type Props = {
+  episode: Episode;
+};
+
+const PlayIcon = (
+  <svg
+    class="ml-[2px] h-2 w-2"
+    fill="none"
+    height="14"
+    viewBox="0 0 13 14"
+    width="13"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      clip-rule="evenodd"
+      d="m.367882.443158c0-.065142.07026-.106046.126866-.073861l11.541952 6.562623c.0573.03256.0573.11515 0 .14772l-11.541949 6.56266c-.056606.0321-.126865-.0088-.126865-.0739z"
+      fill="currentColor"
+      fill-rule="evenodd"
+    />
+  </svg>
+);
+
+const PauseIcon = (
+  <svg
+    class="h-2 w-2"
+    fill="none"
+    height="18"
+    viewBox="0 0 14 18"
+    width="14"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g fill="currentColor">
+      <rect height="16.8" rx="1.07692" width="5.6" y=".799805" />
+      <rect height="16.8" rx="1.07692" width="5.6" x="8.40039" y=".799805" />
+    </g>
+  </svg>
+);
+
+function renderIcon(icon: JSX.Element) {
+  return <span>{icon}</span>;
+}
+
+export default function FullPlayButton({ episode }: Props) {
+  const isCurrentEpisode = episode.id == currentEpisode.value?.id;
+
+  return (
+    <button
+      class="btn"
+      onClick={() => {
+        currentEpisode.value = {
+          ...episode
+        };
+
+        isPlaying.value = isCurrentEpisode ? !isPlaying.value : true;
+      }}
+    >
+      <span class="flex w-full items-center rounded-full p-2 pr-4 text-light-text-heading dark:text-white">
+        <span class="mr-3 flex h-7 w-7 items-center justify-center rounded-full bg-light-text-heading text-black dark:bg-white">
+          {isCurrentEpisode && isPlaying.value
+            ? renderIcon(PauseIcon)
+            : renderIcon(PlayIcon)}
+        </span>
+        {isCurrentEpisode && isPlaying.value ? 'Pause' : 'Play'} Episode
+        <span class="sr-only">
+          (press to {isCurrentEpisode && isPlaying.value ? 'pause)' : 'play)'}
+        </span>
+      </span>
+    </button>
+  );
+}
