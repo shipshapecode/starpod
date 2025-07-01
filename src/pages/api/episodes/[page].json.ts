02d1605494d687e737e954289dd64754662f5ca5
Robert Wagner <rwwagner90@gmail.com>
2025-07-01 00:51:03 -0400

    Try some tweaks

--- src/pages/api/episodes/[page].json.ts
+++ src/pages/api/episodes/[page].json.ts
@@ -1,15 +1,25 @@
 import type { APIRoute } from 'astro';
+import { optimizeEpisodeImage } from '../../../lib/optimize-episode-image';
 import { getAllEpisodes } from '../../../lib/rss';
+import type { Episode } from '../../../lib/rss';
 
 const episodesPerPage = 15;
-const allEpisodes = await getAllEpisodes();
 
 export async function getStaticPaths({ paginate }: { paginate: any }) {
+  let allEpisodes = await getAllEpisodes();
+  allEpisodes = await Promise.all(
+    allEpisodes.map(async (episode: Episode) => {
+      episode.episodeThumbnail = await optimizeEpisodeImage(
+        episode.episodeImage
+      );
+      return episode;
+    })
+  );
+
   return paginate(allEpisodes, { pageSize: episodesPerPage });
 }
 
 export const GET: APIRoute = async ({ props }) => {
-  const page = props.page.currentPage;
-  const canLoadMore = page * episodesPerPage < allEpisodes.length;
+  const canLoadMore = props.page.currentPage < props.page.lastPage;
   return new Response(JSON.stringify({ canLoadMore, episodes: props.page }));
 };
