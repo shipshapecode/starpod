c168e00aed4c7dedbbd8dea78c3c001f33f295eb
Robert Wagner <rwwagner90@gmail.com>
2026-01-16 13:54:57 -0500

    Add CodeRabbit sponsor

--- db/data/sponsors.ts
+++ db/data/sponsors.ts
@@ -1,5 +1,11 @@
 export default [
   {
+    id: 'code-rabbit',
+    name: 'CodeRabbit',
+    img: 'code-rabbit.svg',
+    url: 'https://coderabbit.ai/'
+  },
+  {
     id: 'norlan',
     name: 'Norlan',
     img: 'norlan.svg',
